4a674540f4194a96ed9d9e4acd54d628d7d65d93
wamballa <wamballa@Main-MBP.local>
2019-03-27 12:47:21 +0000

    added Bootstrap CSS and responsiveness

--- src/components/weather/Form.js
+++ src/components/weather/Form.js
@@ -2,15 +2,18 @@ import React from 'react'
 
 export default function Form(props) {
   // const props_ = {props};
-  console.log(props);
+  // console.log(props);
   return (
-    <div>
-      {/* console.log({this.props_}); */}
-      <form onSubmit={props.getWeather}>
-          <input type='text' name='city' placeholder='Enter city...'/>
-          <input type='text' name='country' placeholder='Enter country...'/>
-          <button>Get Weather</button>
+    <div className="m-2">
+      <form className="p-3 input-group mb-3" onSubmit={props.getWeather}>
+        <div className="input-group-prepend"><span className="input-group-text">Location</span></div>
+        <input type="text" name='city' className="form-control" placeholder="City" />
+        <input type="text" name='country' className="form-control" placeholder="Country" />
+        <div className="input-group-append">
+          <button className="btn btn-success" type="submit">Go</button>
+        </div>
       </form>
     </div>
+
   )
 }
--- src/components/weather/WeatherSummary.js
+++ src/components/weather/WeatherSummary.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Form from './Form';
-import Weather from './Weather'
+import WeatherCurrentCard from './WeatherCurrentCard'
 
 const API_KEY = 'e22d1c07fa47b19cb8d862add6c876d5';
 
@@ -9,11 +9,17 @@ export class WeatherSummary extends Component {
   state = {
     city: undefined,
     country: undefined,
+    mainDescription: undefined,
     description: undefined,
+    temp: undefined,
+    tempMax: undefined,
+    tempMin: undefined,
+    windDirection: undefined,
+    windSpeed: undefined,
     icon: undefined,
     error: undefined
   };
-  
+
   getWeather = async (e) => {
     // console.log ("getWeather data before " , this.state);
 
@@ -22,41 +28,55 @@ export class WeatherSummary extends Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    if (data.name && data.sys.country){
+    if (data.name && data.sys.country) {
       this.setState({
         city: data.name,
         country: data.sys.country,
+        mainDescription: data.weather[0].main,
         description: data.weather[0].description,
+        temp: data.main.temp,
+        tempMax: data.main.temp_max,
+        tempMin: data.main.temp_min,
+        windDirection: data.wind.deg,
+        windSpeed: data.wind.speed,
         icon: data.weather[0].icon,
-        error: ""
+        error: ''
       });
     } else {
       this.setState({
         city: undefined,
         country: undefined,
+        mainDescription: undefined,
         description: undefined,
+        temp: undefined,
+        tempMax: undefined,
+        tempMin: undefined,
+        windDirection: undefined,
+        windSpeed: undefined,
         icon: undefined,
         error: 'Error: with choice of City or Country'
       });
     }
-
     // console log all API call data
-    // console.log ("getWeather data raw " , data);
+    // console.log("getWeather data raw ", data);
     // console log state
-    console.log ("getWeather data state " , this.state);
-
-
+    console.log("getWeather data state ", this.state);
   }
 
   render() {
     return (
       <div>
-        <h1>The Weather Summary</h1>
-        <Form getWeather={this.getWeather}/>
-        <Weather 
-          city={this.state.city} 
+        <Form getWeather={this.getWeather} />
+        <WeatherCurrentCard
+          city={this.state.city}
           country={this.state.country}
+          mainDescription={this.state.mainDescription}
           description={this.state.description}
+          temp={this.state.temp}
+          tempMax={this.state.tampMax}
+          tempMin={this.state.tempMin}
+          windDirection={this.state.windDirection}
+          windSpeed={this.state.windSpeed}
           icon={this.state.icon}
           error={this.state.error}
         />
